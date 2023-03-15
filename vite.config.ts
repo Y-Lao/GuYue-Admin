6315b56799cc0bf908e6d0b42cc5f62d4880e16c
Y_Lao <986953860@qq.com>
2023-03-15 23:46:17 +0800

    feat: 项目按需引入AntdV集成

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,48 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+// 按需引入antdV
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // @see: https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  resolve:{
-    alias:{
-      "@": resolve(__dirname, "./src")
-    }
-  },
-  plugins: [vue()],
-  server: {
-    // 启动端口
-    port: 8080,
-    hmr: {
-      host: "127.0.0.1",
-      port: 8080
-    },
-    // 设置https 代理
-    proxy: {
-      "/api": {
-        target: ' https://mock.mengxuegu.com/mock/64112a1afe77f949bc0d6ec6/guyue',
-        changeOrigin: true,
-        rewrite: (path: string) => path.replace(/^\/api/, "")
-      }
-    }
-  }
-})
+	base: "./",
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src")
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			less: {
+				additionalData: `
+          @import "@/styles/theme/global.less";
+        `
+			}
+		}
+	},
+	plugins: [
+		vue(),
+		// UI组件库按需引入
+		Components({
+			resolvers: [AntDesignVueResolver()]
+		})
+	],
+	server: {
+		// 启动端口
+		port: 8080,
+		hmr: {
+			host: "127.0.0.1",
+			port: 8080
+		},
+		// 设置https 代理
+		proxy: {
+			"/api": {
+				target: " https://mock.mengxuegu.com/mock/64112a1afe77f949bc0d6ec6/guyue",
+				changeOrigin: true,
+				rewrite: (path: string) => path.replace(/^\/api/, "")
+			}
+		}
+	}
+});
