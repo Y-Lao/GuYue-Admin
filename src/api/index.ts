3efdaf12d90fd9a1ba7f1b74d0627b966a080665
luoyang <luoyang@jiuwei.cloud>
2023-03-20 17:08:01 +0800

    feat: 项目axios集成

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
-import type { AxiosInstance, AxiosError, AxiosRequestConfig } from "axios";
-
+import type { AxiosInstance, AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { message } from "ant-design-vue";
+import { ResultData } from "@/api/interface";
 import { ResultEnum } from "@/enums/httpEnum";
+import { checkStatus } from "./helper/checkStatus";
 import { GlobalStore } from "@/stores";
 
 // 配置
@@ -37,6 +39,66 @@ class RequestHttp {
 				return Promise.reject(error);
 			}
 		);
+
+		/**
+		 * @description 响应拦截器
+		 *  服务器换返回信息 -> [拦截统一处理] -> 客户端JS获取到信息
+		 */
+		this.service.interceptors.response.use(
+			(response: AxiosResponse) => {
+				const { data } = response;
+				const globalState = GlobalStore();
+				// * 在请求结束后，并关闭请求 loading
+				// 未完成
+				// * 登录失效（code == 401）
+				if (data.code == ResultEnum.OVERDUE) {
+					message.error(data.msg);
+					globalState.setToken("");
+					// 跳转到登录页
+					// 未完成
+					return Promise.reject(data);
+				}
+				//  * 全局错误信息拦截（防止下载文件得时候返回数据流，没有code，直接报错）
+				if (data.code && data.code !== ResultEnum.SUCCESS) {
+					message.error(data.msg);
+					return Promise.reject(data);
+				}
+				// * 成功请求（在页面上除非特殊情况，否则不用在页面处理失败逻辑）
+				return data;
+			},
+			async (error: AxiosError) => {
+				const { response } = error;
+				// 关闭请求 loading
+				// 未完成
+				// 请求超时 && 网络错误单独判断，没有 response
+				if (error.message.indexOf("timeout") !== -1) message.error("请求超时！请您稍后重试");
+				if (error.message.indexOf("Network Error") !== -1) message.error("网络错误！请您稍后重试");
+				// 根据响应的错误状态码，做不同的处理
+				if (response) checkStatus(response.status);
+				// 服务器结果都没有返回(可能服务器错误可能客户端断网)，断网处理:可以跳转到断网页面
+				if (!window.navigator.onLine) {
+					// 跳转500显示页
+				}
+				return Promise.reject(error);
+			}
+		);
+	}
+
+	// * 常用请求方法封装
+	get<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+		return this.service.get(url, { params, ..._object });
+	}
+	post<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+		return this.service.post(url, params, _object);
+	}
+	put<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+		return this.service.put(url, params, _object);
+	}
+	delete<T>(url: string, params?: any, _object = {}): Promise<ResultData<T>> {
+		return this.service.delete(url, { params, ..._object });
+	}
+	download(url: string, params?: object, _object = {}): Promise<BlobPart> {
+		return this.service.post(url, params, { ..._object, responseType: "blob" });
 	}
 }
 
