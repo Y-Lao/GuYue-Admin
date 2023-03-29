93ba2bca78ae696e034a1ddd60fb49e20bf0b509
Y_Lao <986953860@qq.com>
2023-03-29 22:18:46 +0800

    feat: 消息中心

--- src/layouts/components/Header/components/Message.vue
+++ src/layouts/components/Header/components/Message.vue
@@ -1,14 +1,62 @@
 <template>
 	<div class="message">
 		<a-popover placement="bottom" trigger="click">
-			<a-badge count="5" class="item">
+			<a-badge count="6" class="item">
 				<i :class="'iconfont icon-xiaoxi'" class="toolBar-icon"></i>
 			</a-badge>
 			<template #content>
 				<a-tabs v-model:activeKey="activeKey">
-					<a-tab-pane key="inform" tab="Tab 1">Content of Tab Pane 1</a-tab-pane>
-					<a-tab-pane key="2" tab="Tab 2" force-render>Content of Tab Pane 2</a-tab-pane>
-					<a-tab-pane key="3" tab="Tab 3">Content of Tab Pane 3</a-tab-pane>
+					<a-tab-pane key="inform" tab="通知(6)">
+						<div class="message-list">
+							<div class="message-item">
+								<img src="@/assets/images/msg01.png" alt="" class="message-icon" />
+								<div class="message-content">
+									<span class="message-title">一键三连 Geeker-Admin 🧡</span>
+									<span class="message-date">一分钟前</span>
+								</div>
+							</div>
+							<div class="message-item">
+								<img src="@/assets/images/msg02.png" alt="" class="message-icon" />
+								<div class="message-content">
+									<span class="message-title">一键三连 Geeker-Admin 💙</span>
+									<span class="message-date">一小时前</span>
+								</div>
+							</div>
+							<div class="message-item">
+								<img src="@/assets/images/msg03.png" alt="" class="message-icon" />
+								<div class="message-content">
+									<span class="message-title">一键三连 Geeker-Admin 💚</span>
+									<span class="message-date">半天前</span>
+								</div>
+							</div>
+							<div class="message-item">
+								<img src="@/assets/images/msg04.png" alt="" class="message-icon" />
+								<div class="message-content">
+									<span class="message-title">一键三连 Geeker-Admin 💜</span>
+									<span class="message-date">一星期前</span>
+								</div>
+							</div>
+							<div class="message-item">
+								<img src="@/assets/images/msg05.png" alt="" class="message-icon" />
+								<div class="message-content">
+									<span class="message-title">一键三连 Geeker-Admin 💛</span>
+									<span class="message-date">一个月前</span>
+								</div>
+							</div>
+						</div>
+					</a-tab-pane>
+					<a-tab-pane key="message" tab="消息(0)" force-render>
+						<div class="message-empty">
+							<img src="@/assets/images/notData.png" alt="notData" />
+							<div>暂无消息</div>
+						</div>
+					</a-tab-pane>
+					<a-tab-pane key="todo" tab="代办(0)">
+						<div class="message-empty">
+							<img src="@/assets/images/notData.png" alt="notData" />
+							<div>暂无代办</div>
+						</div>
+					</a-tab-pane>
 				</a-tabs>
 			</template>
 		</a-popover>
@@ -18,7 +66,47 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const activeKey = ref("1");
+const activeKey = ref("inform");
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.message-empty {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 284px;
+	height: 260px;
+	line-height: 45px;
+}
+.message-list {
+	display: flex;
+	flex-direction: column;
+	.message-item {
+		display: flex;
+		align-items: center;
+		width: 284px;
+		padding: 20px 0;
+		border-bottom: 1px solid #e4e7ed;
+		&:last-child {
+			border: none;
+		}
+		.message-icon {
+			width: 40px;
+			height: 40px;
+			margin: 0 20px 0 5px;
+		}
+		.message-content {
+			display: flex;
+			flex-direction: column;
+			.message-title {
+				margin-bottom: 5px;
+			}
+			.message-date {
+				font-size: 12px;
+				color: #909309;
+			}
+		}
+	}
+}
+</style>
